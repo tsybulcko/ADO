15aba33fed860882915614550f96bcd65e5a1bc4
roman <roman@ROMANPC>
2026-03-25 22:37:22 +0300

    Connector begin

---
+++ ADO/Connector.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace ADO
+{
+	internal class Connector
+	{
+		string connection_string;
+		SqlConnection connection;
+		public Connector(string connection_string)
+		{
+			Console.WriteLine(connection_string);
+			this.connection_string = connection_string;
+			connection = new SqlConnection(connection_string);
+
+		}
+		public void Select(string cmd)
+		{
+			connection.Open();
+			SqlCommand command = new SqlCommand(cmd, connection);
+			SqlDataReader reader = command.ExecuteReader();
+			for (int i = 0; i < reader.FieldCount; i++)
+				Console.Write(reader.GetName(i) + "\t");
+			Console.WriteLine();
+			while (reader.Read())
+			{
+				//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}\t{reader[3]}");
+				for (int i = 0; i < reader.FieldCount; i++)
+					Console.Write($"{reader[i]}\t\t");
+				Console.WriteLine();
+			}
+			reader.Close();
+			connection.Close();
+		}
+		public object Scalar(string cmd)
+		{
+			object result = null;
+			connection.Open();
+			SqlCommand command = new SqlCommand(cmd, connection);
+			result = command.ExecuteScalar(); //Выполнение скалярного запроса.
+
+			connection.Close();
+			return result;
+		}
+	}
+}
--- ADO/Program.cs
+++ ADO/Program.cs
@@ -11,32 +11,27 @@ namespace ADO
 {
 	internal class Program
 	{
+		
 		static void Main(string[] args)
 		{
 			string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_521;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-			Console.WriteLine(connection_string);
+			
+			Connector connector = new Connector(connection_string);
 
-			SqlConnection connection = new SqlConnection(connection_string);
-			connection.Open();
 
 			string cmd = "SELECT movie_id,title,release_date,first_name,last_name FROM Movies,Directors WHERE director=director_id";
-			SqlCommand command = new SqlCommand(cmd, connection);
-			SqlDataReader reader = command.ExecuteReader();
-			for (int i = 0; i < reader.FieldCount; i++) 
-				Console.Write(reader.GetName(i) + "\t");
-			 Console.WriteLine();
-			while (reader.Read())
-			{
-				//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}\t{reader[3]}");
-				for (int i = 0; i < reader.FieldCount; i++)
-					Console.Write($"{reader[i]}\t\t");
-				Console.WriteLine();
-			}
-			reader.Close();
+			
+			connector.Select(cmd);
+			Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT(*) FROM Movies")}");
+
+			connector.Select("SELECT * FROM Directors");
+			Console.WriteLine($"Количество записей: {connector.Scalar("SELECT COUNT(*) FROM Directors")}");
 		
-			command.CommandText = "SELECT COUNT(*) FROM Movies";
-			Console.WriteLine($"Количетво записей:\t{command.ExecuteScalar()}");
-			connection.Close();
+			//command.CommandText = "SELECT COUNT(*) FROM Movies";
+			//Console.WriteLine($"Количетво записей:\t{command.ExecuteScalar()}");
+			//connection.Close();
 		}
+
+		
 	}
 }
